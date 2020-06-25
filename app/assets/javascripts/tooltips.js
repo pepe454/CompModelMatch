4dacd49a1c0035e2dc4b95ba27b8cceac1344218
sahil <sjha200000@gmail.com>
2020-06-25 18:21:39 +0000

    disabled all popups globally

--- app/assets/javascripts/tooltips.js
+++ app/assets/javascripts/tooltips.js
@@ -1,36 +1,36 @@
-$j(document).ready(function () {
-    bindTooltips('body');
+// $j(document).ready(function () {
+//     bindTooltips('body');
 
-    $j('label.required, span.required').popover({
-        html: false,
-        animation: false,
-        delay: {
-            show: 500,
-            hide: 100
-        },
-        trigger: 'hover',
-        placement: 'auto right',
-        container: 'body',
-        content: 'This field is required.'
-    });
-});
+//     $j('label.required, span.required').popover({
+//         html: false,
+//         animation: false,
+//         delay: {
+//             show: 500,
+//             hide: 100
+//         },
+//         trigger: 'hover',
+//         placement: 'auto right',
+//         container: 'body',
+//         content: 'This field is required.'
+//     });
+// });
 
-function bindTooltips(root_tag) {
-    $j(root_tag + ' [data-tooltip]').popover({
-        html: false,
-        animation: false,
-        trigger: 'hover',
-        placement: 'auto right',
-        container: 'body',
-        delay: {
-            show: 500,
-            hide: 100
-        },
-        content: function () {
-            return $j(this).data('tooltip');
-        }
-    });
-};
+// function bindTooltips(root_tag) {
+//     $j(root_tag + ' [data-tooltip]').popover({
+//         html: false,
+//         animation: false,
+//         trigger: 'hover',
+//         placement: 'auto right',
+//         container: 'body',
+//         delay: {
+//             show: 500,
+//             hide: 100
+//         },
+//         content: function () {
+//             return $j(this).data('tooltip');
+//         }
+//     });
+// };
 
 
 
